7ba0b3a87f48162d8d8bec9266770d1d7d51a9e4
Brian Tran <thebbht@gmail.com>
2023-04-18 22:00:07 -0700

    More IsolationTest, added mysql jar file

--- IsolationTest.java
+++ IsolationTest.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.util.*;
+import java.io.*;
 public class IsolationTest{
     public static void main(String[] args){
         final String sqliteConnection = "jdbc:sqlite:";
@@ -12,6 +13,30 @@ public class IsolationTest{
         try{
             con1 = DriverManager.getConnection(mysqlConnection, mysqlUsername, mysqlPassword);
             con2 = DriverManager.getConnection(mysqlConnection, mysqlUsername, mysqlPassword);
+
+            ResultSet dbNames = con1.getMetaData().getCatalogs();
+            boolean dbExists = false;
+            while(dbNames.next()){
+                String existingDB = dbNames.getString(1);
+                if("ISOLATION_TEST".equals(existingDB)){
+                    dbExists = true;
+                }
+            }
+            dbNames.close();
+            if(!dbExists){
+                FileInputStream dbFile = new FileInputStream("MakeDB.txt");
+                Scanner sc = new Scanner(dbFile);
+                String createDBStmt = "";
+                String createTableStmt = "";
+                while(sc.hasNextLine()){
+                    createDBStmt = sc.nextLine();
+                    createTableStmt = sc.nextLine();
+                }
+                //Statement createDB = con1.createStatement();
+                //createDB.execute(createDBStmt);
+            }
+            con1.close();
+            con2.close();
         }
         catch(Exception e){
             System.out.println(e.getMessage());
